0eca6c236cb2b5117e1bfadab9fce8dcc51e5e84
Antoine Mauboussin <antoine.mauboussin@livecampus.tech>
2022-08-03 10:25:31 +0200

    user update

--- user.test.js
+++ user.test.js
@@ -1,22 +1,25 @@
 const {User} = require('./user.js');
 
-let user1 = new User("a@bc.com","Antoine","Mauboussin",21)
-let user2 = new User("a@bc.com","Antoine","Mauboussin",12)
-let user3 = new User("abc.com","Antoine","Mauboussin",12)
-let user4 = new User("a@bc.com","","Mauboussin",12)
+afterEach(() => {
+    console.log("teardown")
+});
 
 test('user1 validity', () => {
+    let user1 = new User("a@bc.com","Antoine","Mauboussin",21)
     expect(user1.isValid()).toBe(true);
 });
 
 test('user2 validity', () => {
+    let user2 = new User("a@bc.com","Antoine","Mauboussin",12)
     expect(user2.isValid()).toBe(false);
 });
 
 test('user3 validity', () => {
+    let user3 = new User("abc.com","Antoine","Mauboussin",12)
     expect(user3.isValid()).toBe(false);
 });
 
-test('user3 validity', () => {
+test('user4 validity', () => {
+    let user4 = new User("a@bc.com","","Mauboussin",12)
     expect(user4.isValid()).toBe(false);
 });
